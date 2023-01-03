98bc709dc9eee598406405e433846162f025bdb3
pascoalf <up202010522@g.uporto.pt>
2023-01-03 15:25:03 +0000

    Starting first function

--- R/define_rb.R
+++ R/define_rb.R
@@ -1,16 +1,22 @@
 #' Title
 #'
-#' @param x
+#' @param data A tibble with, at least, a column for Abundance and Sample. Additional columns are allowed.
+#' @param classification_vector A vector of strings with the names for each cluster, from lower to higher abundance. Default is c("Rare", "Undetermined", "Abundance")
+#' @param samples_id String with name of column with sample names
+#' @param abundance_id String with name of column with abundance values
 #'
-#' @return
+#' @return The input tibble with additional columns for the cluster and classification.
 #' @export
 #'
-#' @examples
+#' @examples 1
 #'
 #' @import dplyr
 #' @import tidyr
 #' @import cluster
 #' @import purrr
-define_rb <- function(x) {
+define_rb <- function(data,
+                      classification_vector = c("Rare","Undetermined","Abundant"),
+                      samples_id = "Sample",
+                      abundance_id = "Abundance") {
 
 }
