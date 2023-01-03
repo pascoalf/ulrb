15d605973e51f2730e8da7a335332be12e7b1b45
pascoalf <up202010522@g.uporto.pt>
2023-01-03 16:35:43 +0000

    Added test to see if the function runs without error

--- tests/testthat/test-define_rb.R
+++ tests/testthat/test-define_rb.R
@@ -1,3 +1,6 @@
 test_that("Number of clusters", {
   expect_type(define_rb(nice_tidy)$Level, type = "integer")
 })
+test_that("Function runs without error", {
+  expect_no_error(define_rb(nice_tidy))
+})
