780d958ed2f8db63aa7f9df19fd0caec8cce611c
pascoalf <up202010522@g.uporto.pt>
2023-01-04 12:07:01 +0100

    Added multiple tests for define_rb

--- tests/testthat/test-define_rb.R
+++ tests/testthat/test-define_rb.R
@@ -1,6 +1,114 @@
-test_that("Number of clusters", {
-  expect_type(define_rb(nice_tidy)$Level, type = "integer")
-})
 test_that("Function runs without error", {
   expect_no_error(define_rb(nice_tidy))
 })
+test_that("Check if result is reproducible",{
+  expect_equal(define_rb(nice_tidy),
+               define_rb(nice_tidy))
+})
+test_that("Levels output is integer", {
+  expect_type(define_rb(nice_tidy)$Level, type = "integer")
+})
+test_that("Number of levels corresponds to
+          size of classification vector", {
+            test_vector <- c("Rare", "Undetermined","Abundant")
+            expect_equal(
+              length(
+                unique(
+                  define_rb(nice_tidy,
+                            classification_vector = test_vector)$Level)),
+              length(
+                unique(test_vector)))
+            })
+test_that("Number of levels corresponds to
+          size of alternative classification vector", {
+            test_vector <- c("Rare","Abundant")
+            expect_equal(
+              length(
+                unique(
+                  define_rb(nice_tidy,
+                            classification_vector = test_vector)$Level)),
+              length(
+                unique(test_vector)))
+          })
+test_that("Works with a classification vector of a single classification,
+          or a single cluster", {
+            test_vector <- c("Rare")
+            expect_no_error(
+                  define_rb(nice_tidy,
+                            classification_vector = test_vector)$Level)
+          })
+#
+test_that("The median abundance of clusters is type double",{
+  expect_type(define_rb(nice_tidy)$Cluster_median_abundance,
+              type = "double")
+})
+#
+test_that("The classification column is type integer",{
+  expect_type(define_rb(nice_tidy)$Classification,
+              type = "integer")
+})
+test_that("The number of classifications corresponds to classification vector",{
+  classification_vector <- c("Rare", "Undertermined", "Abundant")
+  expect_equal(
+    length(
+      unique(
+        define_rb(nice_tidy, classification_vector = classification_vector)$Classification)),
+    length(
+      unique(classification_vector)))
+})
+test_that("It works with alternative classification vectors",{
+  classification_vector <- c("Rare", "Abundant")
+  expect_no_error(define_rb(nice_tidy,
+                            classification_vector = classification_vector))
+})
+test_that("It works with single classification",{
+  classification_vector <- c("Rare")
+  expect_no_error(define_rb(nice_tidy,
+                            classification_vector = classification_vector))
+})
+test_that("It needs at least one classification to work",{
+  classification_vector <- c()
+  expect_error(define_rb(nice_tidy,
+                            classification_vector = classification_vector))
+})
+test_that("Classification vector can be numbers instead of strings",{
+  classification_vector <- c(1:10)
+  expect_no_error(define_rb(nice_tidy,
+                         classification_vector = classification_vector))
+})
+test_that("Largest possible vector is equal to the number of observations of the sample with least observations",{
+            # Remove zeros and NAs, if any, to get only the valid observations
+            data_cleaned <- filter(nice_tidy, Abundance > 0, !is.na(Abundance))
+
+            # Calculate maximum number of valid observations per sample
+            total_clusters <-
+              dplyr::summarise(group_by(data_cleaned,Sample),
+                               Observation = length(Abundance>0))
+
+            # Get maximum number of clusters
+            maximum_possible_clusters <- min(total_clusters$Observation)-1
+
+            # Make largest classification vector that will work
+            largest_classification_vector <- c(1:(maximum_possible_clusters))
+
+            expect_no_error(define_rb(nice_tidy, classification_vector = largest_classification_vector))
+})
+test_that("The definition does not work for classification vectors with more than the maximum
+          number of possible clusters.",{
+
+            # Remove zeros and NAs, if any, to get only the valid observations
+            data_cleaned <- filter(nice_tidy, Abundance > 0, !is.na(Abundance))
+
+            # Calculate maximum number of valid observations per sample
+            total_clusters <-
+              dplyr::summarise(group_by(data_cleaned,Sample),
+                               Observation = length(Abundance>0))
+
+            # Get maximum number of clusters
+            maximum_possible_clusters <- min(total_clusters$Observation)-1
+
+            # Make classification vector that will not work
+            bad_classification_vector <- c(1:(maximum_possible_clusters+1))
+
+            expect_error(define_rb(nice_tidy, classification_vector = bad_classification_vector))
+          })
