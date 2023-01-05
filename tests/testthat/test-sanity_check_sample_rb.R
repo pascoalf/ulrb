f025a1675aff250c8762ba578aee954225e93d67
pascoalf <up202010522@g.uporto.pt>
2023-01-05 12:09:02 +0000

    Tests under way...

--- tests/testthat/test-sanity_check_sample_rb.R
+++ tests/testthat/test-sanity_check_sample_rb.R
@@ -1,3 +1,11 @@
 test_that("function works", {
-  expect_no_error(sanity_check_rb_sample(define_rb(nice_tidy),sample = "NB_250", taxa_id = "OTU"))
+
+  classified_species <- define_rb(nice_tidy)
+
+  expect_no_error(sanity_check_rb_sample(classified_species, sample = "NB_250", taxa_id = "OTU"))
+})
+#test_that("function fails without sample specification", {
+  classified_species <- define_rb(nice_tidy)
+
+  expect_error(sanity_check_rb_sample(classified_species, taxa_id = "OTU"))
 })
