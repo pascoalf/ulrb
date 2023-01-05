b50a4aced7e1a2d1cb6e728f06f29aacb2712775
pascoalf <up202010522@g.uporto.pt>
2023-01-05 09:39:26 +0000

    Improved documentation of define_rb

--- R/define_rb.R
+++ R/define_rb.R
@@ -1,11 +1,11 @@
-#' Title
+#' Define Rare Biosphere
 #'
 #' @param data A tibble with, at least, a column for Abundance and Sample. Additional columns are allowed.
 #' @param classification_vector A vector of strings with the names for each cluster, from lower to higher abundance. Default is c("Rare", "Undetermined", "Abundance")
 #' @param samples_id String with name of column with sample names
 #' @param abundance_id String with name of column with abundance values
 #'
-#' @return The input tibble with additional columns for the cluster and classification.
+#' @return A tibble with the initial columns and new columns for the cluster and classification of each species.
 #' @export
 #'
 #' @examples 1
