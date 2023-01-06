1a4d74de086c7a3ec08eb1b9cf26b76fe8876093
pascoalf <up202010522@g.uporto.pt>
2023-01-06 11:00:25 +0000

    Corrected a syntax error

--- tests/testthat/test-prepare_tidy_data.R
+++ tests/testthat/test-prepare_tidy_data.R
@@ -1,7 +1,7 @@
 test_that("function works", {
   # get sample names
   sample_names <- c("ERR2044662", "ERR2044663", "ERR2044664",
-                    "ERR2044665", "ERR2044666", "ERR2044667"
+                    "ERR2044665", "ERR2044666", "ERR2044667",
                     "ERR2044668", "ERR2044669", "ERR2044670")
 
   expect_no_error(prepare_tidy_data(nice_clean, sample_names = sample_names))
