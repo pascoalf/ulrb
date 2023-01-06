0c3ca6548ecbd9751628fb17160bcfd5500db3be
pascoalf <up202010522@g.uporto.pt>
2023-01-06 15:24:23 +0000

    added documentation for sanity_check_all function

--- R/sanity_check_all.R
+++ R/sanity_check_all.R
@@ -1,12 +1,36 @@
 #' Sanity check of results for all samples
 #'
+#' @inheritParams sanity_check_sample_rb
+#' @inheritParams prepare_tidy_data
 #' @param data output from sanity_check_sample_rb
+#' @param export_output can be "yes" (default) or "no". If "yes", then a pdf file with all plots is produced, else the plots are not printed into pdf.
+#' @param output_name name of pdf file with ouput. Only works if export_output = "yes".
 #' @param ... other arguments
 #'
 #' @return 1
 #' @export
 #'
 #' @examples 1
-sanity_check_all <- function(data, ...){
-
+sanity_check_all <- function(data,
+                             sample_names,
+                             taxa_id,
+                             sample_id,
+                             classification_id,
+                             abundance_id,
+                             colors = c("#0072B2", "#D55E00", "#CC79A7"),
+                             export_output = "yes",
+                             output_name = "Sanity check figures",
+                             ...){
+    #
+    plot_list <- lapply(sample_names, function(x){
+      sanity_check_sample_rb(data = data,
+                             sample_id = x,
+                             taxa_id = taxa_id,
+                             classification_id = classification_id,
+                             abundance_id = abundance_id,
+                             colors = colors)})
+    #
+    pdf(output_name)
+    lapply(plot_list,print)
+    dev.off()
 }
