d725b63627447a2bd8c7f7ad5649490cc379f433
pascoalf <up202010522@g.uporto.pt>
2023-01-06 11:24:17 +0000

    Documented nice and nice_tidy datasets

--- R/data.R
+++ R/data.R
@@ -1,8 +1,11 @@
-#' SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition
+#' OTU table from SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition
 #'
-#' A raw OTU table from SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition.
-#' Raw reads were processed into OTUs by the MGnify platform (v5). Original .fastq files with
-#' sequences are publicly available in European Nucleotide Archive under accesion number XXXXXX
+#' Table with abundance and taxonomic classification of each OTU.
+#' This OTU table was cleaned (see: XXXX) so that it only includes samples from 16S rRNA amplicon sequencing
+#' and eukaryotes were filtered out. Additionally, we added a column with (OTU) with a ID for each OTU.
+#' Raw reads were processed into OTUs by the MGnify platform (v5).
+#' Original .fastq files with sequences are publicly available in European Nucleotide Archive under accesion number XXXX.
+#' The original raw OTU table is publicly available in MGnify under accession number XXXX.
 #'
 #' @format ## `nice`
 #' A data frame with 524 rows and 17 columns:
@@ -10,27 +13,28 @@
 #'   \item{ERR2044662, "ERR2044663", "ERR2044664","ERR2044665", "ERR2044666", "ERR2044667","ERR2044668", "ERR2044669", "ERR2044670"}{Country name}
 #'   \item{OTU}{Taxonomic unit ID}
 #'   \item{Domain}{Domain level classification of taxonomic unit}
-#'   \item{Phylum}{Domain level classification of taxonomic unit}
-#'   \item{Class}{Domain level classification of taxonomic unit}
-#'   \item{Order}{Domain level classification of taxonomic unit}
-#'   \item{Family}{Domain level classification of taxonomic unit}
-#'   \item{Genus}{Domain level classification of taxonomic unit}
-#'   \item{Species}{Domain level classification of taxonomic unit}
+#'   \item{Phylum}{Phylum level classification of taxonomic unit}
+#'   \item{Class}{Class level classification of taxonomic unit}
+#'   \item{Order}{Order level classification of taxonomic unit}
+#'   \item{Family}{Family level classification of taxonomic unit}
+#'   \item{Genus}{Genus level classification of taxonomic unit}
+#'   \item{Species}{Species level classification of taxonomic unit}
 #'   ...
 #' }
-#' @source <https://www.ebi.ac.uk/metagenomics/api/v1/studies/MGYS00001922/pipelines/5.0/file/ERP024265_taxonomy_abundances_SSU_v5.0.tsv>
+#' @source <https://www.ebi.ac.uk/metagenomics/studies/MGYS00001922#analysis>
 "nice"
 
-#' SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition
+#' Tidy OTU table from SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition
 #'
-#' A raw OTU table from SSU rRNA amplicon of the Arctic Ocean during Winter-Spring Transition.
-#' Raw reads were processed into OTUs by the MGnify platform (v5). Original .fastq files with
-#' sequences are publicly available in European Nucleotide Archive under accesion number XXXXXX
+#' Tidy version of original OTU table (nice).
+#' A new column (Sample) includes the sample identifiers and a new column (Abundance) includes the abundance for each taxonomic unit.
+#' For details on OTU table processing see help page of nice data.
 #'
 #' @format ## `nice_tidy`
-#' A data frame with 524 rows and 17 columns:
+#' A data frame with 4716 rows and 10 columns:
 #' \describe{
-#'   \item{ERR2044662, "ERR2044663", "ERR2044664","ERR2044665", "ERR2044666", "ERR2044667","ERR2044668", "ERR2044669", "ERR2044670"}{Country name}
+#'   \item{Sample}{Sample ID}
+#'   \item{Abundance}{Abundance}
 #'   \item{OTU}{Taxonomic unit ID}
 #'   \item{Domain}{Domain level classification of taxonomic unit}
 #'   \item{Phylum}{Domain level classification of taxonomic unit}
@@ -41,5 +45,5 @@
 #'   \item{Species}{Domain level classification of taxonomic unit}
 #'   ...
 #' }
-#' @source <https://www.ebi.ac.uk/metagenomics/api/v1/studies/MGYS00001922/pipelines/5.0/file/ERP024265_taxonomy_abundances_SSU_v5.0.tsv>
+#' @source <https://www.ebi.ac.uk/metagenomics/studies/MGYS00001922#analysis>
 "nice_tidy"
