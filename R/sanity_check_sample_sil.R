2ddfd2613b9425f3672b1c09df396c36e30510d4
pascoalf <up202010522@g.uporto.pt>
2023-01-09 15:34:57 +0000

    added sanity check for silhouette plots, empty for now

---
+++ R/sanity_check_sample_sil.R
@@ -0,0 +1,12 @@
+#' Sanity check of Silhouette plot of selected sample
+#'
+#' @param data ...
+#'
+#' @return A ggplot object of Silhouette plot obtained from the selected sample.
+#' @export
+#'
+#' @examples
+#' sanity_check_sample_sil(data = define_rb(nice_tidy, simplified = FALSE))
+sanity_check_sample_sil <- function(data){
+
+}
