91480b204c24505189da343c3d3aa651965c9cec
pascoalf <up202010522@g.uporto.pt>
2023-07-20 16:35:39 +0100

    tests for suggest_k

---
+++ tests/testthat/test-suggest_k.R
@@ -0,0 +1,38 @@
+test_that("No error", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662")
+  expect_no_error(suggest_k(sample_ERR2044662))
+})
+
+test_that("No error after removing absent species", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662", Abundance > 0)
+  expect_no_error(suggest_k(sample_ERR2044662))
+})
+
+test_that("Test if it is ok to cange range of values", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662", Abundance > 0)
+  expect_no_error(suggest_k(sample_ERR2044662, range = 20:30))
+})
+
+test_that("Expect error for k = 1", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662")
+  expect_error(suggest_k(sample_ERR2044662, range = 1:10))
+})
+
+test_that("Should work for all possible k's without errors", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662", Abundance > 0)
+  max_k_of_ERR2044662 <- length(unique(sample_ERR2044662$Abundance))
+  expect_no_error(suggest_k(sample_ERR2044662, range = 2:(max_k_of_ERR2044662-1)))
+})
+
+test_that("Should throw error if max k is reached", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662", Abundance >0)
+  max_k_of_ERR2044662 <- length(unique(sample_ERR2044662$Abundance))
+  expect_error(suggest_k(sample_ERR2044662, range = 2:(max_k_of_ERR2044662+1)))
+})
+
+test_that("Input vector as one dimension", {
+  sample_ERR2044662 <- filter(nice_tidy, Sample == "ERR2044662")
+  max_k_of_ERR2044662 <- length(unique(sample_ERR2044662$Abundance))
+  ## a data.frame, instead of a vector, should throw an error
+  expect_error(suggest_k(sample_ERR2044662, range = 2:(max_k_of_ERR2044662+1)))
+})
