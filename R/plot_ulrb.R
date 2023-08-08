07e67c41669d06d3c82131164458985c765161ee
pascoalf <up202010522@g.uporto.pt>
2023-08-08 10:37:02 +0100

    testing new approach for all the necessary plots

---
+++ R/plot_ulrb.R
@@ -0,0 +1,96 @@
+#' Plot relevant metrics obtained from ulrb clustering
+#'
+#' @param data a data.frame with, at least, the classification, abundance and sample information for each taxonomic unit.
+#' @param see What you want to see in the plot. Can be either "clustering" (default), "Silhouette score", "Calinsky-Harabasz" or "Davies-Bouldin".
+#' @param sample_id Default is NULL. If you want to analyze one specific sample, specify its name as a string.
+#' @param taxa_col string with name of column with taxonomic units, e.g., OTU, ASV...
+#' @param classification_col string with name of column with classification for each row. Default value is "Classification".
+#' @param abundance_col string with name of column with abundance values. Default is "Abundance".
+#' @param colors vector with colors. Should have the same length as the number of classifications
+#' @param log_scaled if TRUE then abundance scores will be shown in Log10 scale. Default to FALSE.
+#' @param ... other arguments
+#'
+#' @return a ggplot object
+#' @export
+#'
+#' @examples
+#' classified_species <- define_rb(nice_tidy)
+#'
+#' plot_ulrb(classified_species,
+#'                        sample_id = "ERR2044669",
+#'                        taxa_col = "OTU",
+#'                        abundance_id = "Abundance")
+#'
+#' @import dplyr
+#' @importFrom rlang .data
+plot_ulrb <- function(data,
+                      see = "clustering",
+                      sample_id = NULL,
+                      taxa_col,
+                      classification_col = "Classification",
+                      abundance_id = "Abundance",
+                      colors = c("#0072B2", "#D55E00", "#CC79A7"),
+                      log_scaled = FALSE,
+                      ...){
+  ## Notes to erase afterwards ##
+  # this function will be divided into 2 logical sections
+  # On the one side, it might be used *before* doing the clustering
+  # but it can also be used to verify the results of the clustering.
+
+  # then, further divide into one or several samples
+  ################
+
+  # list conditions
+
+
+  ####
+
+  #
+  if(missing(sample_id)){
+    stop("You must specify one sample from the column with samples ID's.")
+  }
+  if(missing(taxa_col)){
+    stop("You must specify which column includes the taxonomic units.")
+  }
+  if(is.matrix(data))
+    stop("Please use data.frame in tidy format.")
+  if(length(colors) != length(unique(data$Classification))){
+    stop("Number of colors must correspond to number of classifications used.")
+  }
+  if(!is.logical(log_scaled)){
+    stop("'log_scaled' argument needs to be logical (TRUE/FALSE)")
+  }
+
+  # Make sure the taxa_col corresponds to the correct column
+  data <- data %>%
+    rename(ID = all_of(taxa_col),
+           Classification = all_of(classification_col),
+           Abundance = all_of(abundance_id))
+
+  make_plot <- function(){
+    data %>%
+      filter(.data$Sample == sample_id) %>%
+      ggplot2::ggplot(ggplot2::aes(x = reorder(.data$ID, -.data$Abundance),
+                                   .data$Abundance, col = .data$Classification)) +
+      ggplot2::geom_point()+
+      ggplot2::theme(axis.text.x = ggplot2::element_blank(),
+                     axis.ticks.x = ggplot2::element_blank(),
+                     panel.grid = ggplot2::element_blank(),
+                     axis.line.x.bottom = ggplot2::element_line(),
+                     axis.line.y.left = ggplot2::element_line(),
+                     panel.background = ggplot2::element_blank(),
+                     legend.position = "top")+
+      ggplot2::scale_color_manual(values = colors)+
+      ggplot2::labs(title = paste("Rank Abundance Curve for ", sample_id),
+                    x = taxa_col)
+  }
+
+  if(isTRUE(log_scaled)){
+    intermediate_plot <- make_plot()
+    intermediate_plot +
+      ggplot2::scale_y_log10()+
+      ggplot2::labs(y = "Abundance in Log10 scale")
+  } else {
+    make_plot()
+  }
+}
